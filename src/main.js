e001d26c34096791d4fdb9f7f31de14bdb72dcf3
ltizzle12 <blah@blah.com>
2023-11-13 18:07:50 -0600

    Lab 11 updated!

--- src/main.js
+++ src/main.js
@@ -3,10 +3,10 @@ import { createPinia } from 'pinia'
 import './style.css'
 import App from './App.vue'
 
-createApp(App).mount('#app')
+// createApp(App).mount('#app')
 
 const app = createApp(App)
 const pinia = createPinia()
 app.use(pinia)
-app.mount('app')
+app.mount('#app') // CSS Selector #app
 // this will install and run pinia
